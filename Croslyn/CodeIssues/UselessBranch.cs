2da5c5452bb7b7c5f6628516d5d3ebaa97725f53
Strilanc <craig.gidney@gmail.com>
2012-05-04 13:52:39 -0300

    UselessBranch code issue

---
+++ Croslyn/CodeIssues/UselessBranch.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Threading;
+using Roslyn.Compilers;
+using Roslyn.Compilers.CSharp;
+using Roslyn.Services;
+using Roslyn.Services.Editor;
+using Strilbrary.Collections;
+using Roslyn.Compilers.Common;
+using System.Diagnostics.Contracts;
+using Strilbrary.Values;
+
+namespace Croslyn.CodeIssues {
+    [ExportSyntaxNodeCodeIssueProvider("Croslyn", LanguageNames.CSharp, typeof(IfStatementSyntax))]
+    internal class UselessBranch : ICodeIssueProvider {
+        private readonly ICodeActionEditFactory editFactory;
+
+        [ImportingConstructor]
+        internal UselessBranch(ICodeActionEditFactory editFactory) {
+            this.editFactory = editFactory;
+        }
+
+        public IEnumerable<CodeIssue> GetIssues(IDocument document, CommonSyntaxNode node, CancellationToken cancellationToken) {
+            var statement = (IfStatementSyntax)node;
+            var newStatement = statement.DropEmptyBranchesIfApplicable();
+            if (newStatement == statement) return null;
+            
+            var r = new ReadyCodeAction("Omit useless code", editFactory, document, statement, () => newStatement);
+            return r.CodeIssues1(CodeIssue.Severity.Warning, statement.IfKeyword.Span, "Useless branch");
+        }
+        public IEnumerable<CodeIssue> GetIssues(IDocument document, CommonSyntaxToken token, CancellationToken cancellationToken) {
+            throw new NotImplementedException();
+        }
+        public IEnumerable<CodeIssue> GetIssues(IDocument document, CommonSyntaxTrivia trivia, CancellationToken cancellationToken) {
+            throw new NotImplementedException();
+        }
+    }
+}
