f281902c8141773649c90a3398d29b7c331a591e
Strilanc <craig.gidney@gmail.com>
2012-05-13 23:10:53 -0300

    Cleanup in ForEachProject

--- Croslyn/CodeIssues/ForEachProject.cs
+++ Croslyn/CodeIssues/ForEachProject.cs
@@ -24,31 +24,33 @@ namespace Croslyn.CodeIssues {
 
         public IEnumerable<CodeIssue> GetIssues(IDocument document, CommonSyntaxNode node, CancellationToken cancellationToken) {
             var forLoop = (ForEachStatementSyntax)node;
-            var model = document.TryGetSemanticModel();
-            if (model == null) return null;
+            var model = document.GetSemanticModel();
 
-            // loop uses iterator once, projecting it?
-            var sym = model.GetDeclaredSymbol(forLoop);
-            var read = forLoop.Statement.DescendentNodes()
-                       .OfType<SimpleNameSyntax>()
-                       .Where(e => model.GetSemanticInfo(e).Symbol == sym)
-                       .SingleOrDefaultAllowMany();
-            if (read == null) return null;
-            var projection = read.Ancestors()
+            // loop uses iterator once?
+            var declaredSymbol = model.GetDeclaredSymbol(forLoop);
+            var singleRead = forLoop.Statement.DescendentNodes()
+                             .OfType<SimpleNameSyntax>()
+                             .Where(e => model.GetSemanticInfo(e).Symbol == declaredSymbol)
+                             .SingleOrDefaultAllowMany();
+            if (singleRead == null) return null;
+            
+            // safe iterator projection?
+            var projection = singleRead.Ancestors()
                              .TakeWhile(e => e is ExpressionSyntax)
                              .Cast<ExpressionSyntax>()
                              .TakeWhile(e => e.HasSideEffects(model) <= Analysis.Result.FalseIfCodeFollowsConventions)
                              .LastOrDefault();
-            if (projection == null || projection == read) return null;
+            if (projection == null) return null;
+            if (projection.TryGetAlternativeEquivalence(singleRead, model) == true) return null;
 
+            // build replacement loop
             var projectedCollection = forLoop.Expression
                                       .Accessing("Select")
                                       .Invoking(forLoop.Identifier.Lambdad(projection).Args1());
-            var newBody = forLoop.Statement.ReplaceNode(projection, read);
-            var replacedLoop = forLoop.With(
-                expression: projectedCollection,
-                statement: newBody);
+            var newBody = forLoop.Statement.ReplaceNode(projection, singleRead);
+            var replacedLoop = forLoop.With(expression: projectedCollection, statement: newBody);
 
+            // expose as code action/issue
             var action = new ReadyCodeAction(
                 "Project collection",
                 editFactory,
